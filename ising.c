18b935712a2f8150cffe1e43a9fe056507e50d2d
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-25 08:10:10 -0300

    Try to improve performance

--- ising.c
+++ ising.c
@@ -18,16 +18,16 @@ void update(const float temp, int grid[L][L]) {
     exp_table[(8)+8] = expf(8 / temp);
 
   // typewriter update
-  for (unsigned int i = 0; i < L; ++i) {
-    for (unsigned int j = 0; j < L; ++j) {
-      int spin_old = grid[j][i];
+  for (unsigned int j = 0; j < L; ++j) {
+    for (unsigned int i = 0; i < L; ++i) {
+      int spin_old = grid[i][j];
       int spin_new = (-1) * spin_old;
 
       // computing h_before
-      int spin_neigh_n = grid[j][(i + L - 1) % L];
-      int spin_neigh_e = grid[(j + 1) % L][i];
-      int spin_neigh_w = grid[(j + L - 1) % L][i];
-      int spin_neigh_s = grid[j][(i + 1) % L];
+      int spin_neigh_n = grid[(i + L - 1) % L][j];
+      int spin_neigh_e = grid[i][(j + 1) % L];
+      int spin_neigh_w = grid[i][(j + L - 1) % L];
+      int spin_neigh_s = grid[(i + 1) % L][j];
       int h_before = -(spin_old * spin_neigh_n) - (spin_old * spin_neigh_e) -
                      (spin_old * spin_neigh_w) - (spin_old * spin_neigh_s);
 
@@ -38,7 +38,7 @@ void update(const float temp, int grid[L][L]) {
       int delta_E = h_after - h_before;
       float p = random();
       if (delta_E <= 0 || p <= exp_table[-delta_E+8]) {
-        grid[j][i] = spin_new;
+        grid[i][j] = spin_new;
       }
     }
   }
@@ -49,11 +49,11 @@ double calculate(int grid[L][L], int *M_max) {
   int E = 0;
   for (unsigned int i = 0; i < L; ++i) {
     for (unsigned int j = 0; j < L; ++j) {
-      int spin = grid[j][i];
-      int spin_neigh_n = grid[j][(i + L - 1) % L];
-      int spin_neigh_e = grid[(j + 1) % L][i];
-      int spin_neigh_w = grid[(j + L - 1) % L][i];
-      int spin_neigh_s = grid[j][(i + 1) % L];
+      int spin = grid[i][j];
+      int spin_neigh_n = grid[(i + 1) % L][j];
+      int spin_neigh_e = grid[i][(j + 1) % L];
+      int spin_neigh_w = grid[i][(j + L - 1) % L];
+      int spin_neigh_s = grid[(i + L - 1) % L][j];
 
       E += (spin * spin_neigh_n) + (spin * spin_neigh_e) +
            (spin * spin_neigh_w) + (spin * spin_neigh_s);
