a82d6c22a30932bcb74f7aa8ee92b314ba1612d8
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-25 08:17:39 -0300

    Try to improve performance

--- ising.h
+++ ising.h
@@ -1,6 +1,6 @@
 #include "params.h"
 #include <stdint.h>
 
-void update(const float temp, int8_t grid[L][L]);
+void update(const float temp, int_fast8_t grid[L][L]);
 
-double calculate(int8_t grid[L][L], int *M_max);
+double calculate(int_fast8_t grid[L][L], int *M_max);
