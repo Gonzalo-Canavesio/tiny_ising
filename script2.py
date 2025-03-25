e1ce30437c328e3cd78b091baf53851ca6d6a033
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-25 03:23:37 -0300

    Add clang to script 2 to verify best optimization

--- script2.py
+++ script2.py
@@ -1,8 +1,8 @@
 import subprocess
 from time import time
 
-CC = ["gcc"]
-CFLAGS = ["-std=c11", "-Wall", "-Wextra", "-march=native"]
+CC = ["clang"]
+CFLAGS = ["-std=c11", "-Wall", "-Wextra"]
 EXTRAFLAGS = ["-lgomp", "-lm"]
 FILES = ["ising.c", "tiny_ising.c", "xoshiro256plus.c"]
 
@@ -11,26 +11,28 @@ O_LIST = ["O1", "O2", "O3", "Ofast", "Os"]
 
 for line_size in PARAMS_LIST:
     for o in O_LIST:
-        best_performance = 0.0
 
-        cmd = CC + CFLAGS + FILES + EXTRAFLAGS + [f"-{o}"] + [f"-DL={line_size}"]
+        for m in ["-march=native", ""]:
+            best_performance = 0.0
 
-        compilation_result = subprocess.run(cmd, stderr=subprocess.PIPE, text=True)
-        if compilation_result.stderr:
-            print(compilation_result.stderr)
+            cmd = CC + CFLAGS + FILES + EXTRAFLAGS + [f"-{o}"] + [m] + [f"-DL={line_size}"]
 
-        duration = 3 * 60
-        start = time()
-        while time() - start < duration:
-            execution_result = subprocess.run(
-                ["./a.out"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-            )
-            for line in execution_result.stdout.splitlines():
-                if line.startswith("# Spins/ms:"):
-                    best_performance = max(
-                        best_performance, float(line.replace("# Spins/ms:", ""))
-                    )
+            compilation_result = subprocess.run(cmd, stderr=subprocess.PIPE, text=True)
+            if compilation_result.stderr:
+                print(compilation_result.stderr)
 
-        print(f"L: {line_size}")
-        print(f"Optimization: {o}")
-        print(f"Spins/ms: {best_performance}")
+            duration = 3 * 60
+            start = time()
+            while time() - start < duration:
+                execution_result = subprocess.run(
+                    ["./a.out"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+                )
+                for line in execution_result.stdout.splitlines():
+                    if line.startswith("# Spins/ms:"):
+                        best_performance = max(
+                            best_performance, float(line.replace("# Spins/ms:", ""))
+                        )
+
+            print(f"L: {line_size}")
+            print(f"Optimization: {o} {m}")
+            print(f"Spins/ms: {best_performance}")
