d786db4a56b603b39463cd28abe81275e4752874
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-24 22:56:04 -0300

    Improve script to run experiments

---
+++ script.py
@@ -0,0 +1,33 @@
+import subprocess
+from time import time
+
+CC = ["gcc"]
+CFLAGS = ["-std=c99", "-Wall", "-Wextra"]
+EXTRAFLAGS = ["-lgomp", "-lm"]
+FILES = ["ising.c", "tiny_ising.c"]
+
+PARAMS_LIST = [2,4,8,16,32,64,128,217, 256,384,512,1024,1031,2048,4096,8192]
+
+for line_size in PARAMS_LIST:
+
+    best_performance = 0.0
+
+    cmd = CC + CFLAGS + FILES + EXTRAFLAGS + [f"-DL={line_size}"]
+
+    compilation_result = subprocess.run(cmd, stderr=subprocess.PIPE, text=True)
+    if compilation_result.stderr:
+        print(compilation_result.stderr)
+
+    duration = 3 * 60
+    start = time()
+    while time() - start < duration:
+        execution_result = subprocess.run(
+            ["./a.out"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+        for line in execution_result.stdout.splitlines():
+            if line.startswith("# Spins/ms:"):
+                best_performance = max(best_performance, float(line.replace("# Spins/ms:", "")))
+
+    print(f"L: {line_size}")
+    print(f"Spins/ms: {best_performance}")
+
