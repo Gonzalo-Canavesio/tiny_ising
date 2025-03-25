e558977c0175f25fb95164ebe2e927e95e6cf815
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-25 06:21:16 -0300

    try to improve performance with use of type 1 byte int

--- ising.h
+++ ising.h
@@ -1,5 +1,6 @@
 #include "params.h"
+#include <stdint.h>
 
 void update(const float temp, int8_t grid[L][L]);
 
-double calculate(int8_t grid[L][L], int* M_max);
+double calculate(int8_t grid[L][L], int *M_max);
