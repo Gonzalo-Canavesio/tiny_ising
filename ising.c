4fbb7442d0d314f01a8f2ce649f73c3d5962a486
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-25 08:31:25 -0300

    Try to improve performance

--- ising.c
+++ ising.c
@@ -8,6 +8,7 @@
 
 void update(const float temp, int_fast8_t grid[L][L]) {
   float exp_table[32];
+
   exp_table[(-8) + 8] = expf(-8 / temp);
   exp_table[(-4) + 8] = expf(-4 / temp);
   exp_table[(-2) + 8] = expf(-2 / temp);
@@ -16,40 +17,28 @@ void update(const float temp, int_fast8_t grid[L][L]) {
   exp_table[(4) + 8] = expf(4 / temp);
   exp_table[(8) + 8] = expf(8 / temp);
 
-  // Block size (typically chosen to fit in cache)
-  const int BLOCK_SIZE = 8;
+  // typewriter update
+  for (unsigned int i = 0; i < L; ++i) {
+    for (unsigned int j = 0; j < L; ++j) {
+      int_fast8_t spin_old = grid[i][j];
+      int_fast8_t spin_new = (-1) * spin_old;
 
-  // Iterate over blocks
-  for (unsigned int block_i = 0; block_i < L; block_i += BLOCK_SIZE) {
-    for (unsigned int block_j = 0; block_j < L; block_j += BLOCK_SIZE) {
-      // Inner loop over each block
-      for (unsigned int i = block_i; i < (block_i + BLOCK_SIZE < L ? block_i + BLOCK_SIZE : L); ++i) {
-        for (unsigned int j = block_j; j < (block_j + BLOCK_SIZE < L ? block_j + BLOCK_SIZE : L); ++j) {
-          int_fast8_t spin_old = grid[i][j];
-          int_fast8_t spin_new = (-1) * spin_old;
+      // computing h_before
+      int_fast8_t spin_neigh_n = grid[(i + L - 1) % L][j];
+      int_fast8_t spin_neigh_e = grid[i][(j + 1) % L];
+      int_fast8_t spin_neigh_w = grid[i][(j + L - 1) % L];
+      int_fast8_t spin_neigh_s = grid[(i + 1) % L][j];
+      int_fast8_t h_before = -(spin_old * spin_neigh_n) - (spin_old * spin_neigh_e) -
+                       (spin_old * spin_neigh_w) - (spin_old * spin_neigh_s);
 
-          // Computing neighboring spins with periodic boundary conditions
-          int_fast8_t spin_neigh_n = grid[(i + L - 1) % L][j];
-          int_fast8_t spin_neigh_e = grid[i][(j + 1) % L];
-          int_fast8_t spin_neigh_w = grid[i][(j + L - 1) % L];
-          int_fast8_t spin_neigh_s = grid[(i + 1) % L][j];
+      // h after taking new spin
+      int_fast8_t h_after = -(spin_new * spin_neigh_n) - (spin_new * spin_neigh_e) -
+                      (spin_new * spin_neigh_w) - (spin_new * spin_neigh_s);
 
-          // Calculate energy before and after spin flip
-          int_fast8_t h_before =
-              -(spin_old * spin_neigh_n) - (spin_old * spin_neigh_e) -
-              (spin_old * spin_neigh_w) - (spin_old * spin_neigh_s);
-          int_fast8_t h_after =
-              -(spin_new * spin_neigh_n) - (spin_new * spin_neigh_e) -
-              (spin_new * spin_neigh_w) - (spin_new * spin_neigh_s);
-
-          int_fast8_t delta_E = h_after - h_before;
-          float p = random();
-
-          // Metropolis criterion
-          if (delta_E <= 0 || p <= exp_table[-delta_E + 8]) {
-            grid[i][j] = spin_new;
-          }
-        }
+      int_fast8_t delta_E = h_after - h_before;
+      float p = random();
+      if (delta_E <= 0 || p <= exp_table[-delta_E + 8]) {
+        grid[i][j] = spin_new;
       }
     }
   }
