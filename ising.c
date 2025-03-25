79a0e26ba498af902b927183da95a298217d8fbb
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-25 08:08:23 -0300

    Try to improve performance

--- ising.c
+++ ising.c
@@ -20,7 +20,7 @@ void update(const float temp, int grid[L][L]) {
   // typewriter update
   for (unsigned int i = 0; i < L; ++i) {
     for (unsigned int j = 0; j < L; ++j) {
-      int spin_old = grid[i][j];
+      int spin_old = grid[j][i];
       int spin_new = (-1) * spin_old;
 
       // computing h_before
@@ -49,11 +49,11 @@ double calculate(int grid[L][L], int *M_max) {
   int E = 0;
   for (unsigned int i = 0; i < L; ++i) {
     for (unsigned int j = 0; j < L; ++j) {
-      int spin = grid[i][j];
-      int spin_neigh_n = grid[(i + 1) % L][j];
-      int spin_neigh_e = grid[i][(j + 1) % L];
-      int spin_neigh_w = grid[i][(j + L - 1) % L];
-      int spin_neigh_s = grid[(i + L - 1) % L][j];
+      int spin = grid[j][i];
+      int spin_neigh_n = grid[j][(i + L - 1) % L];
+      int spin_neigh_e = grid[(j + 1) % L][i];
+      int spin_neigh_w = grid[(j + L - 1) % L][i];
+      int spin_neigh_s = grid[j][(i + 1) % L];
 
       E += (spin * spin_neigh_n) + (spin * spin_neigh_e) +
            (spin * spin_neigh_w) + (spin * spin_neigh_s);
