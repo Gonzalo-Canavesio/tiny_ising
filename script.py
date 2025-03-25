f699b13d931d0a8ea0034d8be8c19a0e5307978a
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2025-03-25 04:44:39 -0300

    Actualizado codigo script para usar clang

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import subprocess
 from time import time
 
 CC = ["clang"]
-CFLAGS = ["-std=c11", "-Wall", "-Wextra", "-Ofast"]
+CFLAGS = ["-std=c11", "-Wall", "-Wextra", "-Ofast", "-march=native"]
 EXTRAFLAGS = ["-lgomp", "-lm"]
 FILES = ["ising.c", "tiny_ising.c", "xoshiro256plus.c"]
 
